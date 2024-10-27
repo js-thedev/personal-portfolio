2ca499695386cb1f2238f16d86017a3d24b5504c
escapingmatr <js.scott.park@gmail.com>
2024-10-27 19:51:13 -0400

    installed eslint, prettier, sass, font awesome, babel

--- personal-portfolio-app/vite.config.js
+++ personal-portfolio-app/vite.config.js
@@ -1,7 +1,33 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import { fileURLToPath } from 'url';
+import { dirname, resolve } from 'path';
 
-// https://vite.dev/config/
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [
+    react({
+      babel: {
+        presets: ['@babel/preset-react'],
+        plugins: [
+          [
+            'module-resolver',
+            {
+              root: ['./'],
+              alias: {
+                '@': resolve(__dirname, './src'),
+              },
+            },
+          ],
+        ],
+      },
+    }),
+  ],
+  resolve: {
+    alias: {
+      '@': resolve(__dirname, './src'),
+    },
+  },
+});
