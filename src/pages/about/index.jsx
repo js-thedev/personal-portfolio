9b15f1a6f3e40700812a9215c3e45e1418655224
escapingmatr <js.scott.park@gmail.com>
2024-10-29 00:30:44 -0400

    Contents for About page

--- src/pages/about/index.jsx
+++ src/pages/about/index.jsx
@@ -1,2 +1,68 @@
 import '@/pages/index.scss';
 import '@/pages/about/index.scss';
+
+function About() {
+  return (
+    <div className="about">
+      <div className="education">
+        <div className="university">University of Toronto</div>
+        <div className="degree">BASc in Industrial Engineering</div>
+        <div className="education-years">2014-2019</div>
+      </div>
+      <div className="work-experiences">
+        <div className="work-experience">
+          <div className="job-years">2022 - Present</div>
+          <div className="company-name">SAS</div>
+          <div className="job-title">DevOps Engineer</div>
+          <div className="job-descriptions">
+            <div className="job-description">
+              Responsible for deriving and deploying client-specific solutions,
+              via on-premise, and cloud services, such as AWS, GCP, Azure.
+            </div>
+            <div className="job-description">
+              Automate, and manage cloud resources and deployments using
+              Terraform.
+            </div>
+            <div className="job-description">
+              Contributed to critical guidelines and standard documentation,
+              leading to 60% increase in department-wide engineer efficiency in
+              Viya solutions.
+            </div>
+            <div className="job-description">
+              Actively engaged in direct client discussion, transforming
+              gathered requirements to form robust solutions with other seniors
+              and colleagues.
+            </div>
+          </div>
+        </div>
+        <div className="work-experience">
+          <div className="job-years">2022 - Present</div>
+          <div className="company-name">Toronto Hydro</div>
+          <div className="job-title">Software Engineering Intern</div>
+          <div className="job-descriptions">
+            <div className="job-description">
+              Implemented an automation tool that automatically categorizes
+              client/customer invoices, decreasing the amount of time to conduct
+              such categorization by 98%.
+            </div>
+            <div className="job-description">
+              Implemented an automation tool that generates, and delivers Excel
+              reports, presenting the various financial, operative,
+              client-related summaries of the CX department’s weekly
+              accomplishments.
+            </div>
+            <div className="job-description">
+              The automation tool mentioned above drastically improved
+              department-wide efficiency, by decreasing the amount of time
+              required to form the internal report by 95%, resulting in an offer
+              for a full-time position, along with notable acknowledgement
+              across multiple teams and departments.
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default About;
