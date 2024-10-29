1e7b20486fcef809c1be82b315daff2f0f9abd75
Andy Jung <wontaethedev@gmail.com>
2024-10-29 10:56:19 +0900

    Refactor and rename elements in App for clarity

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,20 @@
 import '@/App.scss';
+import '@/assets/styles/common-styles.scss';
+
 import profile from '@/assets/Landing-Profile.jpg';
 
 function App() {
   return (
     <div className="app">
-      <div className="Nav">Navigation Bar Spaceholder</div>
-      <div className="wide-screen-container">
-        <img src={profile} alt="Landing-Profile.jpg"></img>
-        <div className="text-container">
+      <div className="nav">Navigation Bar Spaceholder</div>
+
+      <div className="landing-page">
+        <img
+          className="full-width-img"
+          src={profile}
+          alt="Landing-Profile.jpg"
+        />
+        <div className="intro">
           <div className="name">SCOTT PARK</div>
           <div className="role">DEV-OPS ENGINEER</div>
           <div className="summary">
