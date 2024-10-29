832fb5a68d44efe8f123cb8e7772892db95ff9ed
escapingmatr <js.scott.park@gmail.com>
2024-10-28 20:31:10 -0400

    adjusted landing page style to be responsive

--- src/App.jsx
+++ src/App.jsx
@@ -5,13 +5,15 @@ function App() {
   return (
     <div className="app">
       <div className="Nav">Navigation Bar Spaceholder</div>
-      <img src={profile} alt="Landing-Profile.jpg"></img>
-      <div className="text-container">
-        <div className="name">SCOTT PARK</div>
-        <div className="role">DEV-OPS ENGINEER</div>
-        <div className="summary">
-          BUILDS AUTOMATION TO OPTIMIZE SOFTWARE DEVELOPMENT AND DEPLOYMENT
-          PROCESSES
+      <div className="wide-screen-container">
+        <img src={profile} alt="Landing-Profile.jpg"></img>
+        <div className="text-container">
+          <div className="name">SCOTT PARK</div>
+          <div className="role">DEV-OPS ENGINEER</div>
+          <div className="summary">
+            BUILDS AUTOMATION TO OPTIMIZE SOFTWARE DEVELOPMENT AND DEPLOYMENT
+            PROCESSES
+          </div>
         </div>
       </div>
     </div>
