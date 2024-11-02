cebb291843aa9f32816904ef02ea0d762c731f60
escapingmatr <js.scott.park@gmail.com>
2024-11-02 00:13:59 -0400

    navbar completed

--- src/components/header/nav-bar/index.jsx
+++ src/components/header/nav-bar/index.jsx
@@ -1,35 +1,88 @@
-import { Link, NavLink } from 'react-router-dom';
+import { useState } from 'react';
+import { Link } from 'react-router-dom';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faBars, faTimes } from '@fortawesome/free-solid-svg-icons';
 
 import '@/components/header/nav-bar/index.scss';
 
 function NavBar() {
+  const [isMobileMenuOpen, setIsMobileMenuOpen] = useState(false);
+
+  // Toggle the mobile menu
+  const toggleMobileMenu = () => {
+    setIsMobileMenuOpen(!isMobileMenuOpen);
+  };
+
+  // Close the mobile menu when a link is clicked
+  const closeMobileMenu = () => {
+    setIsMobileMenuOpen(false);
+  };
+
   return (
-    <div className="nav-bar">
+    <div
+      className={`nav-bar ${isMobileMenuOpen ? 'mobile-menu-active' : ''}`}
+      aria-label="Main Navigation"
+    >
       <div className="home-container">
-        <Link to="/" className="home normal-link">
+        <Link to="/" className="home normal-link" onClick={closeMobileMenu}>
           Scott
         </Link>
       </div>
       <div className="nav-link-container">
         <Link to="/about" className="nav-link normal-link">
-          about
+          About
         </Link>
         <Link to="/projects" className="nav-link normal-link">
-          projects
+          Projects
         </Link>
         <Link to="/skills" className="nav-link normal-link">
-          skills
+          Skills
         </Link>
         <Link to="/contact" className="nav-link normal-link">
-          contact
+          Contact
         </Link>
       </div>
       {/* TODO: implement hamburger to open up sidebar for navbar links */}
-      <div className="hamburger">
-        <FontAwesomeIcon icon={faBars} />
+      <div
+        className="hamburger"
+        onClick={toggleMobileMenu}
+        aria-label="Toggle navigation menu"
+        aria-expanded={isMobileMenuOpen}
+      >
+        <FontAwesomeIcon icon={isMobileMenuOpen ? faTimes : faBars} />
       </div>
+      {isMobileMenuOpen && (
+        <div className="mobile-menu">
+          <Link
+            to="/about"
+            className="nav-link normal-link"
+            onClick={closeMobileMenu}
+          >
+            About
+          </Link>
+          <Link
+            to="/projects"
+            className="nav-link normal-link"
+            onClick={closeMobileMenu}
+          >
+            Projects
+          </Link>
+          <Link
+            to="/skills"
+            className="nav-link normal-link"
+            onClick={closeMobileMenu}
+          >
+            Skills
+          </Link>
+          <Link
+            to="/contact"
+            className="nav-link normal-link"
+            onClick={closeMobileMenu}
+          >
+            Contact
+          </Link>
+        </div>
+      )}
     </div>
   );
 }
